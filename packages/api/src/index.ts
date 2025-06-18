e40578b2b391ccbf84fe845cecb8828e41131a7f
Elliot Braem <elliot@ejlbraem.com>
2025-06-18 14:44:39 -0500

    add global declaration

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,2 +1,11 @@
 // See tsup.config.ts for additional banner/footer js
+import * as NearGlobal from "./near";
 export * from "./near.js";
+
+declare global {
+  interface Window {
+    near: typeof NearGlobal;
+
+    // $$: typeof NearGlobal.utils.convertUnit;
+  }
+}
